5280ad8c3ece0d303caa5f7f1ef6d6b44e467841
Justin Poliey <justin@livelike.com>
2025-08-06 23:40:11 -0400

    Add note in test script about dependency on MITRE Datalog

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Depends on MITRE Datalog or equivalent interpreter for datalog command
+# https://datalog.sourceforge.net/
 set -eu
 diff <(python path.py | sort) <(datalog path.dl | sort)
 diff <(python cli.py path.dl | sort) <(datalog path.dl | sort)
