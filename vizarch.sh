e18faa0f49af8ab1487a3ee21b5197ad064ed774
Justin Poliey <justin@livelike.com>
2025-08-06 23:55:01 -0400

    Differentiate datalog modules from stdlib modules in diagram

--- vizarch.sh
+++ vizarch.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 grep -E '^(import|from)' *.py | perl -lne '
-BEGIN {print "digraph {"}
+BEGIN {
+    print "digraph {";
+    print "cli, path [shape=box];";
+    print "database, evaluate, expr, parse, unify [shape=box color=blue];";
+}
 /^([a-z]+)\.py:from ([a-z_]+)/ && {print $1, " -> ", $2, ";"}; 
 /^([a-z]+)\.py:import ([a-z_]+)/ && {print $1, " -> ", $2, ";"};
 END {print "}"}
