bedee112ec8e33692c1c360426117898cc5c2a54
Justin Poliey <justin@livelike.com>
2025-08-16 22:54:09 -0400

    Allow running a program as an argument from the cli
    The argument is run after any files and before the interactive mode all
    in the same interpreter context so that use cases like interactively
    querying a set of facts and rules from the cli is possible, e.g.:
    
        $ python cli.py path.dl -e 'edge(A,b)?'

--- cli.py
+++ cli.py
@@ -14,8 +14,17 @@ def run_file(dl, file):
     try:
         program = parse_program(file.read())
     except ParseFailure:
-        sys.stderr.write(f"{file.name}: parse error\n")
+        sys.stderr.write(f"file: {file.name}: parse error\n")
         sys.exit(1)
+    run(dl, program)
+
+
+def run_eval(dl, commands):
+    try:
+        program = parse_program(commands)
+    except ParseFailure:
+        sys.stderr.write("eval: parse error\n")
+        sys.exit(2)
     run(dl, program)
 
 
@@ -39,12 +48,15 @@ def run_repl(dl):
 if __name__ == '__main__':
     ap = argparse.ArgumentParser()
     ap.add_argument('file', nargs='?', type=argparse.FileType('r'))
+    ap.add_argument('-e', '--eval')
     ap.add_argument('-i', '--interactive', action='store_true', default=False)
     args = ap.parse_args()
-    if args.file or args.interactive:
+    if args.file or args.eval or args.interactive:
         dl = Engine()
         if args.file:
             run_file(dl, args.file)
+        if args.eval:
+            run_eval(dl, args.eval)
         if args.interactive:
             run_repl(dl)
     else:
